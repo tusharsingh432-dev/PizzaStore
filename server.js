c69e58c958afe35443e6139251de0163009a5f3e
Tushar Singh <tusharsingh432@gmail.com>
2022-01-22 09:54:17 +0530

    publish attempt

--- server.js
+++ server.js
@@ -17,12 +17,17 @@ app.use('/api/pizzas', pizzaRouter);
 app.use('/api/users', userRouter);
 app.use('/api/payments', paymentRouter);
 
-if (process.env.NODE_ENV == "production") {
-    app.use('/', express.static("client/build"));
-    app.use('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '/client/build/index.html'));
-    })
-}
+// if (process.env.NODE_ENV == "production") {
+//     app.use('/', express.static("client/build"));
+//     app.use('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, '/client/build/index.html'));
+//     })
+// }
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 8000;
 
